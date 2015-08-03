0520bb7725df81aee03d4a5947fbb1d9f46ffdfe
Nathan Stier <nathan.stier@gmail.com>
2015-08-02 21:18:00 -0500

    setup jshint

--- spec/answers/index.js
+++ spec/answers/index.js
@@ -1,6 +1,154 @@
-Exer.defining1 = function () {  //
-//================================//
+/*---------------------------------Data Sets---------------------------------*/
+/*****************************************************************************\
+Data Sets 1 
+1.) Create a function named `dataOne`.
+2.) `dataOne` should accept one argument that will be an array.
+3.) `dataOne` should log the 3rd, 5th, 8th and last element of the array
+Note: The last element of an array can be found at the index `Array.length - 1` 
+\*****************************************************************************/
 
-  return 'VICTORY';
 
-};  // end Exer.defining1
+/*****************************************************************************\
+Data Sets 2 
+1.) Create a function named `dataTwo`.
+2.) `dataTwo` should accept one argument that will be an array.
+3.) `dataTwo` should use a for loop to log each element of the array.
+Note: You may either use `Array.length` or the `in` keyword to do this.
+\*****************************************************************************/
+
+
+/*---------------------------------Properties--------------------------------*/
+/*****************************************************************************\
+Properties
+1.) Create a function named `properties`.
+2.) `properties` should accept one argument.
+3.) The argument passed will have the properties `.barkSound`, `.legs`, and
+    `.isAGoodBoy`.
+4.) Log the following properties of the argument: `.legs` and `.isAGoodBoy`.
+\*****************************************************************************/
+
+
+/*-----------------------------------Methods---------------------------------*/
+/*****************************************************************************\
+Methods
+1.) Create a function named `properties`.
+2.) `properties` should accept one argument.
+3.) The argument passed will have the methods `.setBark()`, `.getBark()`,
+    `.bark()`, and `.isAGoodBoy()`.
+Note: `.setBark()` sets a value for private member we can call `.barkSound`
+      using the argument passed to it.
+Note: `.getBark()` gets and returns a value for private member we can call
+      `.barkSound`.
+Note `.isAGoodBoy` sets a value for private member we can call `.goodBoy` and
+     if it recieves an argument sets `.goodBoy` to that value.
+Note `.bark()` will log the current `.barkNoise`.
+4.) Call `.bark()`.
+5.) If `.isAGoodBoy()` returns true when given no arguments call `.setBark()`
+    with the argument 'arf...'.
+6.) Else `.setBark()` with the argument 'whimper'.
+7.) Log the results of calling `.getBark()`.
+8.) Call `.bark()`.
+9.) Call `.isAGoodBoy()` with the argument `true`. 
+\*****************************************************************************/
+
+
+/*-----------------------------------Objects---------------------------------*/
+/*****************************************************************************\
+1.) Create a function called `objects`
+2.) Within the function create a variable called `barkSound` set to 'BARK!'.
+4.) Within the function create an object and store it in the variable `fido`.
+5.) `fido` should have a property `.isAgoodBoy` set to true.
+6.) `fido` should have a property `.getBark()` that is a function that returns
+    the value of `barkSound`.
+7.) `fido` should have a property `.setBark()` that is a function that accepts
+    an argument and sets `barkSound` to the argument's value.
+8.) 
+/*****************************************************************************/
+// Mutability
+// Objects as Maps
+// Arrayology
+// Strings and Their Properties
+// The Arguments Object
+// Math Object
+// Global Object
+
+/* !!DANGER ZONE!! */
+/*  DO NOT CROSS!  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Seriously, go back!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Exer = {};
+for (value in this)
+	Exer[value] = this[value]; 
+module.exports = Exer;
