f3cacc01475c16811590b810346ce2d4f9dc165b
Nathan Stier <nathan.stier@gmail.com>
2015-08-05 14:45:31 -0500

    New code to scrape student answers

--- spec/answers/index.js
+++ spec/answers/index.js
@@ -1,53 +1,73 @@
 /*-----------------------------    DATA SETS    -----------------------------*/
 
-/*****************************    Data Sets 1    *****************************\
+/*****************************    Data Sets 1    *****************************/
+
+var dataOne = function() {
+
+};
 
 
+/*****************************    Data Sets 2    *****************************/
 
-/*****************************    Data Sets 2    *****************************\
+var dataTwo = function() {
 
-
+};
 
 
 /*-----------------------------    PROPERTIES    ----------------------------*/
 
+var properties = function() {
 
+};
 
 
 /*-------------------------------    METHODS    -----------------------------*/
 
+var methods = function() {
 
+};
 
 /*-------------------------------    OBJECTS    -----------------------------*/
 
+var objects = function() {
 
+};
 
 /*----------------------------    MUTABILITY    -----------------------------*/
 
+var mutability = function() {
 
+};
 
 /*--------------------------    OBJECTS AS MAPS    --------------------------*/
 
+var maps = function() {
 
+};
 
 /*----------------------------    ARRAYOLOGY    -----------------------------*/
 
+var arrayology = function() {
 
+};
 
 /*-------------------    STRINGS AND THEIR PROPERTIES    --------------------*/
 
+var strings = function() {
 
+};
 
 /*------------------------   THE ARGUMENTS OBJECT    ------------------------*/
 
+var arguments = function() {
 
+};
 
 /*--------------------------    THE MATH OBJECT    --------------------------*/
 
+var math = function() {
 
-
-/*-------------------------    THE GLOBAL OBJECT    -------------------------*/
-
+};
 
 
 
@@ -128,12 +148,19 @@
 
 
 
+if (typeof Exer === 'undefined') Exer = {};
 
+if (typeof dataOne !== 'undefined') Exer.dataOne = dataOne;
+if (typeof dataTwo !== 'undefined') Exer.dataTwo = dataTwo;
+if (typeof properties !== 'undefined') Exer.properties = properties;
+if (typeof methods !== 'undefined') Exer.methods = methods;
+if (typeof objects !== 'undefined') Exer.objects = objects;
+if (typeof mutability !== 'undefined') Exer.mutability = mutability;
+if (typeof maps !== 'undefined') Exer.maps = maps;
+if (typeof arrayology !== 'undefined') Exer.arrayology = arrayology;
+if (typeof strings !== 'undefined') Exer.strings = strings;
+if (typeof arguments === 'function') Exer.arguments = arguments;
+if (typeof math !== 'undefined') Exer.math = math;
 
-
-
-
-if(!Exer) Exer = {};
-for (value in this)
-	Exer[value] = this[value]; 
-if(module) module.exports = Exer;
+if (typeof module === 'undefined') module = {};
+module.exports = Exer;
